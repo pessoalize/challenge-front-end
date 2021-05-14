ec400b54be8e898d7cd5aa0e099e3e4c4f971c21
Felipe P Covatti <felipepcovatti@gmail.com>
2021-05-14 20:28:25 -0300

    Listen to websocket and prepare to list products

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from 'react'
+import socket from '../../services/socket'
+
+type Product = {
+  _id: string
+  amount: number
+  picture: string
+  name: string
+  price: number
+  description: string
+}
+
 export function Dashboard() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    socket.on('GET_ALL_PRODUCTS', (products: Product[]) => {
+      setProducts(products)
+    })
+  }, [])
+
   return (
-    <div>Dashboard</div>
+    <>
+      <h1>Products</h1>
+      <div>
+        {products.map((product) => (
+          <div key={product._id}>
+            <img src={product.picture} alt="" />
+            Name: {product.name}
+            Price: {product.price}
+            Amount: {product.amount}
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
